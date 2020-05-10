906efe0d81766c79f36b12368deaa80160c766c5
Jady Zarzur <jadyzarzur@gmail.com>
2020-05-10 12:16:35 -0300

    Initial Commit

---
+++ Ask The Oracle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ask The Oracle
+//
+//  Created by Placebo on 10/05/20.
+//  Copyright © 2020 Jady Zarzur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
